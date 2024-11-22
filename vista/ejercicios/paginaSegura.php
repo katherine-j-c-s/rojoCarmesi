3d2b673c683f19d4ac3f5b0778d35fef3b9534db
katherine-j-c-s <kathijcs@gmail.com>
2024-11-21 23:03:17 -0300

    arregle pagina segura

--- vista/ejercicios/paginaSegura.php
+++ vista/ejercicios/paginaSegura.php
@@ -1,26 +1,28 @@
 <?php
 include_once '../../configuracion.php';
 $sesion = new session();
-$datos = data_submitted();
-$objUsuario = $sesion->getObjUsuario();
-$objRolActivo = $sesion->getRolActivo();
-if (!$sesion->activa()) {
+
+if ($sesion->activa()) {
+    include_once '../estructura/cabeceraSegura.php';
+    
+    $datos = data_submitted();
+    $objUsuario = $sesion->getObjUsuario();
+    $objRolActivo = $sesion->getRolActivo();
+
+    if (isset($_GET['Message'])) {
+        print '<script type="text/javascript">alert("' . $_GET['Message'] . '");</script>';
+    }
+    echo "</br></br></br></br></br></br>";
+    
+    echo "<h4 class='alert alert-success'>Bienvenido:  {$objUsuario->getUsNombre()} <br>
+     Su Rol Actual: {$sesion->getRolActivo()->getRolDescripcion()}</h4>";
+}else{
     echo "no hay sesion activa";
     header('Location: login.php');
-}else{
-    header('Location: ./login.php');
+    
 }
-if (isset($_GET['Message'])) {
-    print '<script type="text/javascript">alert("' . $_GET['Message'] . '");</script>';
-}
-echo "</br></br></br></br></br></br>";
-
-  echo "<h4 class='alert alert-success'>Bienvenido:  {$objUsuario->getUsNombre()} <br>
- Su Rol Actual: {$sesion->getRolActivo()->getRolDescripcion()}</h4>";
-
 
 ?>
 <?php
-
 include_once '../estructura/footer.php';
 ?>
